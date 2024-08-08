f07f4048e21a1a6befe6814051928f3e345a8b9e
Seang-G <mgw7730@gmail.com>
2024-08-08 17:07:34 +0900

    1

--- src/test/java/com/coco/board/domain/UserServiceTest.java
+++ src/test/java/com/coco/board/domain/UserServiceTest.java
@@ -1,8 +1,26 @@
 package com.coco.board.domain;
 
-import org.junit.jupiter.api.Test;
 
-@Test
+import com.coco.board.application.dto.UserDto;
+import com.coco.board.infrastructure.persistence.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class UserServiceTest {
+    @Mock
+    private UserRepository repository;
+
+    @Spy
+    private BCryptPasswordEncoder encoder;
+
+    @Test
+    public void userJoin_WhenJoin_ShouldSaveUser() {
+        // Given
+        UserDto userDto = new UserDto();
+
+    }
 
 }
